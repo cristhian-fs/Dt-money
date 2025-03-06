0f06d33a6c41fdb21c358fba1775adb1f931590b
Cristhian <cristhianfernandolp@gmail.com>
2025-03-06 17:56:30 -0300

    chore: map transactions from context to render on TransactionsTable

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -2,33 +2,33 @@ import { Header } from "@/components/Header"
 import { Summary } from "@/components/Summary"
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles"
 import { SearchForm } from "./components/SearchForm"
+import { useContext } from "react"
+import { TransactionsContext, type Transactions } from "@/contexts/TransactionsContext"
+
+
 
 export function Transactions(){
+
+  const { transactions } = useContext(TransactionsContext)
+
   return (
     <>
       <Header />
       <Summary />
-
       <TransactionsContainer>
         <SearchForm />
         <TransactionsTable>
           <tbody>
-            <tr>
-              <td width="50%">Desenvolvimento de website</td>
-              <td>
-                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
-              </td>
-              <td>Venda</td>
-              <td>13/04/2022</td>
-            </tr>
-            <tr>
-              <td width="50%">Hamburger</td>
-              <td>
-                <PriceHighlight variant="outcome">- R$ 59,00</PriceHighlight>
-              </td>
-              <td>Alimentação</td>
-              <td>13/04/2022</td>
-            </tr>
+            {transactions.map((transaction) => (
+               <tr key={transaction.id}>
+                <td width="50%">{transaction.description}</td>
+                <td>
+                  <PriceHighlight variant={transaction.type}>{transaction.price}</PriceHighlight>
+                </td>
+                <td>{transaction.category}</td>
+                <td>{transaction.createdAt}</td>
+              </tr>
+            ))}
           </tbody>
         </TransactionsTable>
       </TransactionsContainer>
